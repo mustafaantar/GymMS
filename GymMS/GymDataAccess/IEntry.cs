979b40bb16b1e8b59ff3fb872b4f9ac38d5fafb6
omarzayton <waledEK2015@gmail.com>
2026-05-04 05:26:48 -0700

    #Create data access layer #Create IEntry Interface #Create BaseEntry base class

---
+++ GymMS/GymDataAccess/IEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccess
+{
+    public interface IEntity
+    {
+        //Abstruct interface
+        //Includes Abstruct methods
+        //Load object data by ID (primaryy key) of each table type
+        void LoadById(int id);
+        //Add data row into a table in the database
+        void AddtoDB();
+
+        //Edit object data in a table in the database
+        void UpdateInDB();
+    }
+}
