f3448da74802580b994109ef75952484c7ed86cf
dianashadood <HASSAN.ABD.2589@gmail.com>
2026-05-05 08:40:52 -0700

    #creating user list form #creating user data form #creating login form

---
+++ GymMS/GymMS/frmUserList.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GymMS
+{
+    partial class frmUserList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_data = new System.Windows.Forms.DataGridView();
+            this.userId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bn_add = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_data
+            // 
+            this.dgv_data.AllowUserToAddRows = false;
+            this.dgv_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_data.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.userId,
+            this.username,
+            this.password,
+            this.userType});
+            this.dgv_data.Location = new System.Drawing.Point(12, 38);
+            this.dgv_data.Name = "dgv_data";
+            this.dgv_data.RowHeadersWidth = 15;
+            this.dgv_data.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_data.Size = new System.Drawing.Size(568, 345);
+            this.dgv_data.StandardTab = true;
+            this.dgv_data.TabIndex = 0;
+            // 
+            // userId
+            // 
+            this.userId.HeaderText = "User ID";
+            this.userId.Name = "userId";
+            this.userId.ReadOnly = true;
+            // 
+            // username
+            // 
+            this.username.HeaderText = "Username";
+            this.username.Name = "username";
+            this.username.Width = 150;
+            // 
+            // password
+            // 
+            this.password.HeaderText = "Password";
+            this.password.Name = "password";
+            this.password.Width = 150;
+            // 
+            // userType
+            // 
+            this.userType.HeaderText = "User type";
+            this.userType.Name = "userType";
+            this.userType.Width = 150;
+            // 
+            // tb_search
+            // 
+            this.tb_search.Location = new System.Drawing.Point(62, 12);
+            this.tb_search.Name = "tb_search";
+            this.tb_search.Size = new System.Drawing.Size(130, 20);
+            this.tb_search.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search:";
+            // 
+            // bn_add
+            // 
+            this.bn_add.Location = new System.Drawing.Point(475, 10);
+            this.bn_add.Name = "bn_add";
+            this.bn_add.Size = new System.Drawing.Size(105, 23);
+            this.bn_add.TabIndex = 3;
+            this.bn_add.Text = "Add";
+            this.bn_add.UseVisualStyleBackColor = true;
+            // 
+            // frmUserList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 391);
+            this.Controls.Add(this.bn_add);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_search);
+            this.Controls.Add(this.dgv_data);
+            this.Name = "frmUserList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User list";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn password;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userType;
+        private System.Windows.Forms.TextBox tb_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bn_add;
+    }
+}
---
+++ GymMS/GymMS/frmUserdata.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GymMS
+{
+    partial class frmUserdata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.bn_save = new System.Windows.Forms.Button();
+            this.cb_type = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bn_close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username:";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(76, 64);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(119, 20);
+            this.tb_password.TabIndex = 5;
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(76, 38);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(119, 20);
+            this.tb_username.TabIndex = 6;
+            // 
+            // bn_save
+            // 
+            this.bn_save.Location = new System.Drawing.Point(121, 138);
+            this.bn_save.Name = "bn_save";
+            this.bn_save.Size = new System.Drawing.Size(75, 23);
+            this.bn_save.TabIndex = 4;
+            this.bn_save.Text = "Save";
+            this.bn_save.UseVisualStyleBackColor = true;
+            // 
+            // cb_type
+            // 
+            this.cb_type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_type.FormattingEnabled = true;
+            this.cb_type.Location = new System.Drawing.Point(76, 90);
+            this.cb_type.Name = "cb_type";
+            this.cb_type.Size = new System.Drawing.Size(119, 21);
+            this.cb_type.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Type:";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(76, 12);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.ReadOnly = true;
+            this.tb_id.Size = new System.Drawing.Size(119, 20);
+            this.tb_id.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ID:";
+            // 
+            // bn_close
+            // 
+            this.bn_close.Location = new System.Drawing.Point(12, 138);
+            this.bn_close.Name = "bn_close";
+            this.bn_close.Size = new System.Drawing.Size(75, 23);
+            this.bn_close.TabIndex = 4;
+            this.bn_close.Text = "Close";
+            this.bn_close.UseVisualStyleBackColor = true;
+            // 
+            // frmUserdata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 173);
+            this.Controls.Add(this.cb_type);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.bn_close);
+            this.Controls.Add(this.bn_save);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmUserdata";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Button bn_save;
+        private System.Windows.Forms.ComboBox cb_type;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bn_close;
+    }
+}
