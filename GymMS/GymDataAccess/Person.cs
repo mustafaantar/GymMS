fe05ac2b55b59783c4ffb86ba9ce8e00bc15c9de
omarzayton <waledEK2015@gmail.com>
2026-05-05 04:02:03 -0700

    #Creating person class

---
+++ GymMS/GymDataAccess/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccess
+{
+    public abstract class Person : BaseEntity
+    {
+        //variable members
+        string fullName;
+        string phoneNumber;
+        DateTime? birthDate;
+
+        // Properties for Encapsulation
+        public string FullName
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public DateTime? BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public Person() : base() { }
+
+        public Person(int id)
+        {
+            LoadById(id);
+        }        
+    }
+}
