c98b6dc74b82083081d7ce08a86412a1842e28d1
safaabadalsayed <girltalaat@gmail.com>
2026-05-05 10:06:14 -0700

    #create static variables class "Global Variables" # include logged in user in GlobalVariable

---
+++ GymMS/GymMS/GlobalVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMS
+{
+    static class GlobalVariables
+    {
+
+        static GymDataAccess.Users loginUser;
+        static GymDataAccess.Users LoginUser { get { return loginUser; } }
+        static void InitailizeLoginUser(GymDataAccess.Users u)
+        {
+            loginUser = u;
+        }
+    }
+}
