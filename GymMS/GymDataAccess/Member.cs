80d978c261f05f5b00467942d8643827876840a5
omarzayton <waledEK2015@gmail.com>
2026-05-05 04:37:30 -0700

    #

--- GymMS/GymDataAccess/Member.cs
+++ GymMS/GymDataAccess/Member.cs
@@ -21,5 +21,70 @@ namespace GymDataAccess
             LoadById(id);
         }
 
+        public override void LoadById(int id)
+        {
+            SqlCommand comm = new SqlCommand("SELECT * FROM members WHERE id=" + id, GymDBConnection);
+
+            GymDBConnection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+
+            if (reader.Read())
+            {
+
+                Id = (int)reader["id"];
+                FullName = reader["full_name"].ToString();
+                PhoneNumber = reader["phone_pumber"].ToString();
+                BirthDate = reader["birth_date"] as DateTime?;
+                startDate = (DateTime)reader["start_date"];
+                endDate = reader["end_date"] as DateTime?;
+                CreatedBy = (int)reader["created_by"];
+                CreationDate = (DateTime)reader["creation_date"];
+            }
+        }
+
+        public override void AddtoDB()
+        {
+            try
+            {
+                SqlCommand comm = new SqlCommand(@"insert into members 
+            (full_name, phone_pumber, birth_date, start_date, created_by, end_date)
+            values (@name,@phone,@birthDate,@startDate,@createdBy,@endDate)", GymDBConnection);
+
+                comm.Parameters.AddWithValue("@name", FullName);
+                comm.Parameters.AddWithValue("@phone", (object)PhoneNumber ?? DBNull.Value);
+                comm.Parameters.AddWithValue("@birthDate", (object)BirthDate ?? DBNull.Value);
+                comm.Parameters.AddWithValue("@startDate", StartDate);
+                comm.Parameters.AddWithValue("@createdBy", CreatedBy);
+                comm.Parameters.AddWithValue("@endDate", (object)EndDate ?? DBNull.Value);
+
+                GymDBConnection.Open();
+                comm.ExecuteNonQuery();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { GymDBConnection.Close(); }
+        }
+
+        public override void UpdateInDB()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(@"update members set
+            full_name=@name, phone_pumber=@phone, birth_date=@birthDate,
+            start_date=@startDate, end_date=@endDate
+            where id=@id", GymDBConnection);
+
+                cmd.Parameters.AddWithValue("@id", Id);
+                cmd.Parameters.AddWithValue("@name", FullName);
+                cmd.Parameters.AddWithValue("@phone", (object)PhoneNumber ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@birthDate", (object)BirthDate ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@startDate", StartDate);
+                cmd.Parameters.AddWithValue("@endDate", (object)EndDate ?? DBNull.Value);
+
+                GymDBConnection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { GymDBConnection.Close(); }
+        }
     }
 }
