8eb1d3483c93f25c430eefde613ed10b5e30e922
omarzayton <waledEK2015@gmail.com>
2026-05-05 05:30:41 -0700

    #creating trainer class

---
+++ GymMS/GymDataAccess/Trainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccess
+{
+    public class Trainer : Person
+    {
+        string specialization;
+        decimal salary;
+
+        public string Specialization { get { return specialization; } }
+        public decimal Salary { get { return salary; } }
+
+        public Trainer() { }
+
+        public Trainer(int id)
+        {
+            LoadById(id);
+        }
+
+    }
+}
