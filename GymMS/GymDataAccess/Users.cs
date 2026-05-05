61bc3d3399b1144830ea1c8bb68a9ee78b8dc84a
omarzayton <waledEK2015@gmail.com>
2026-05-05 08:20:01 -0700

    #create user class

---
+++ GymMS/GymDataAccess/Users.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccess
+{
+    public class Users : BaseEntity
+    {
+        // Variable members
+        string username;
+        string password;
+        int userType;
+        bool isActive;
+
+        // Properties
+        public string Username { get { return username; } set { username = value; } }
+        public string Password { get { return password; } set { password = value; } }
+        public int UserType { get { return UserType; } set { UserType = value; } }
+        public bool IsActive { get { return isActive; } }
+
+        public Users() { }
+
+        public Users(int id)
+        {
+            LoadById(id);
+        }
+
+        // 🔷 Load single user
+        public override void LoadById(int id)
+        {
+            SqlCommand comm = new SqlCommand("select * from users where id=@id", GymDBConnection);
+
+            comm.Parameters.AddWithValue("@id", id);
+
+            try
+            {
+                GymDBConnection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Id = (int)reader["id"];
+                    username = reader["username"].ToString();
+                    password = reader["password"].ToString();
+                    userType = (int)reader["user_type"];
+                    isActive = (bool)reader["is_active"];
+                    CreatedBy = (int)reader["created_by"];
+                    CreationDate = (DateTime)reader["creation_date"];
+                }
+            }
+            finally { GymDBConnection.Close(); }
+        }
+
+        //methods
+        public override void AddtoDB()
+        {
+            //method to add data into database
+            try
+            {
+                SqlCommand comm = new SqlCommand(@"INSERT INTO users (username, password, user_type, is_active, created_by)
+            VALUES (@user,@pass,@name,@active,@createdBy)", GymDBConnection);
+
+                comm.Parameters.AddWithValue("@user", Username);
+                comm.Parameters.AddWithValue("@pass", Password);
+                comm.Parameters.AddWithValue("@user_type", userType);
+                comm.Parameters.AddWithValue("@active", IsActive);
+                comm.Parameters.AddWithValue("@createdBy", CreatedBy);
+
+                GymDBConnection.Open();
+                comm.ExecuteNonQuery();
+            }
+            finally { GymDBConnection.Close(); }
+        }
+
+        public override void UpdateInDB()
+        {
+            //method to edit data into database
+            try
+            {
+                SqlCommand cmd = new SqlCommand(@"UPDATE users SET
+            username=@username, password=@password, user_type=@user_type, is_active=@active
+            WHERE id=@id", GymDBConnection);
+
+                cmd.Parameters.AddWithValue("@id", Id);
+                cmd.Parameters.AddWithValue("@username", Username);
+                cmd.Parameters.AddWithValue("@password", Password);
+                cmd.Parameters.AddWithValue("@user_type", userType);
+                cmd.Parameters.AddWithValue("@active", IsActive);
+
+                GymDBConnection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally { GymDBConnection.Close(); }
+        }
+
+        
+    }
+}
