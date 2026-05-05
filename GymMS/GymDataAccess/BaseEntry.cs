d856fb7f68295cee8230362813965b96bab23b2e
omarzayton <waledEK2015@gmail.com>
2026-05-05 04:17:38 -0700

    #creating data access layer #IEntry interface #BaseEntry base class

---
+++ GymMS/GymDataAccess/BaseEntry.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+using System.Data.SqlClient;
+using System.Reflection;
+namespace GymDataAccess
+{
+    public abstract class BaseEntity : IEntity
+    {
+        //SQL connection
+        static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="+ Assembly.GetExecutingAssembly() +"\\GymDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public static SqlConnection GymDBConnection = new SqlConnection(connectionString);
+
+        //Shared variable members
+        int id;
+        int createdBy;
+        DateTime creationDate;
+        public int Id { get { return id; } protected set { id = value; } }
+        public int CreatedBy { get { return createdBy; } protected set { createdBy = value; } }
+        public DateTime CreationDate { get { return creationDate; } protected set { creationDate = value; } }
+
+        public BaseEntity()
+        {
+            
+        }
+
+        public abstract void LoadById(int id);
+        public abstract void AddtoDB();
+        public abstract void UpdateInDB();
+    }
+}
