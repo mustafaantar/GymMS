0541a65c5c4b653475bc9fd4d76d7f3c80b3f4d5
omarzayton <waledEK2015@gmail.com>
2026-05-05 04:21:01 -0700

    #creating member class

---
+++ GymMS/GymDataAccess/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccess
+{
+    public class Member : Person
+    {
+        DateTime startDate;
+        DateTime? endDate;
+        public DateTime StartDate { get { return startDate; } }
+        public DateTime? EndDate { get { return endDate; } }
+
+        public Member() { }
+
+        public Member(int id)
+        {
+            LoadById(id);
+        }
+
+    }
+}
