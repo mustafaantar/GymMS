ef880cee8dc8b5fe65e175d6e4fde3653766f18b
omarzayton <waledEK2015@gmail.com>
2026-05-06 12:01:58 -0700

    #add list persons method

--- GymMS/GymDataAccess/Person.cs
+++ GymMS/GymDataAccess/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,6 +41,63 @@ namespace GymDataAccess
         public Person(int id)
         {
             LoadById(id);
-        }        
+        }
+
+        public static List<Person> ListData(string filter)
+        {
+            //create empty list
+            List<Person> list = new List<Person>();
+
+            //prepare select statement
+            string str = "select * from v_persons";
+
+            //if filter used add it to select statement
+            if ((filter != null) && (filter != ""))
+                str += "where fullname like '%' + @filter + '%'";
+
+            //preparing command
+            SqlCommand comm = new SqlCommand(str, GymDBConnection);
+
+            //add filter to command
+            comm.Parameters.AddWithValue("@filter", string.IsNullOrEmpty(filter) ? (object)DBNull.Value : filter);
+
+            try
+            {
+                //open connection
+                GymDBConnection.Open();
+
+                //Execute select statement
+                SqlDataReader reader = comm.ExecuteReader();
+
+                //if data exists read it
+                while (reader.Read())
+                {
+                    Person p;
+
+                    //create member or trainer object depending on person type
+                    if (reader["person_type"].ToString() == "Member")
+                        p = new Member();
+                    else
+                        p = new Trainer();
+
+                    //Assign data into object
+                    p.Id = (int)reader["id"];
+                    p.FullName = reader["full_name"].ToString();
+                    p.PhoneNumber = reader["phone_number"].ToString();
+                    p.BirthDate = (DateTime)reader["birth_date"];
+                    p.CreatedBy = (int)reader["created_by"];
+                    p.CreationDate = (DateTime)reader["creation_date"];
+
+                    //Add object to list
+                    list.Add(p);
+                }
+            }
+            //any exception throw it to up level
+            catch (Exception ex) { throw ex; }
+            //close connection
+            finally { GymDBConnection.Close(); }
+
+            return list;
+        }
     }
 }
