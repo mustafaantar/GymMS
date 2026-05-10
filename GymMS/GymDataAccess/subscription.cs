f41193ca76e7fb56daadaf1099faf98454d5c933
omarzayton <waledEK2015@gmail.com>
2026-05-10 03:46:41 -0700

    #fixing conditions in ListDate

--- GymMS/GymDataAccess/subscription.cs
+++ GymMS/GymDataAccess/subscription.cs
@@ -154,7 +154,7 @@ namespace GymDataAccess
             finally { con.Close(); }
         }
 
-        public static List<Subscription> ListData(int? memberId, int? typeId, DateTime? fromDate, DateTime? toDate)
+        public static List<Subscription> ListData(int memberId, int? typeId, DateTime? fromDate, DateTime? toDate)
         {
             //prepare connection
             SqlConnection con = GymDBConnection;
@@ -167,7 +167,7 @@ namespace GymDataAccess
                 //prepare the select statement
                 string str = "select * from subscriptions "
                 + "where member_id = @memberId"
-                + " and subscription_type = @TypeId"
+                + " and (subscription_type = @TypeId and @TypeId is null)"
                 + " and (start_date >= @fromDate or @fromDate is null)"
                 + " and (start_date <= @toDate or @toDate is null)";
 
